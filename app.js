b57b60acf5af1a676debee48706dd8ca2baeb276
zaguipedro03-stack <zaguipedro03@gmail.com>
2026-09-13 23:05:19 -0300

    Projeto Node

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// ========================================
+// CONFIGURAÇÃO DO EJS
+// ========================================
+
+app.set("view engine", "ejs");
+
+app.set(
+    "views",
+    path.join(__dirname, "views")
+);
+
+
+// ========================================
+// MIDDLEWARES
+// ========================================
+
+// Permite receber dados enviados por formulário
+app.use(express.urlencoded({ extended: true }));
+
+// Permite receber requisições com JSON
+app.use(express.json());
+
+
+// ========================================
+// ROTAS
+// ========================================
+
+const categoriaRoutes = require("./routes/categoriaRoutes");
+
+app.use("/categorias", categoriaRoutes);
+
+const clienteRoutes = require("./routes/clienteRoutes");
+
+app.use("/clientes", clienteRoutes)
+
+const fornecedorRoutes = require("./routes/fornecedorRouter");
+
+app.use("/fornecedores", fornecedorRoutes)
+
+const produtoRoutes = require("./routes/produtoRoutes");
+
+app.use("/produtos", produtoRoutes);
+// ========================================
+// ROTA PRINCIPAL
+// ========================================
+
+app.get("/", (req, res) => {
+
+    res.render("index");
+
+});
+
+
+// ========================================
+// SERVIDOR
+// ========================================
+
+app.listen(3000, () => {
+
+    console.log(
+        "Servidor rodando em http://localhost:3000"
+    );
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+})
